dab2f8011e491fd8d7745d4baf1f9eeae8a120f7
Fahmid Imtiaz <fahmidimtiaz13@gmail.com>
2023-02-04 23:09:51 +0600

    first-day

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./about.css";
+
+export const About = () => {
+  return (
+    <div className="about">
+      <div className="about-left">about-left</div>
+      <div className="about-right">about-right</div>
+    </div>
+  );
+};
